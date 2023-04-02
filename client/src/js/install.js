7059e5517f54f12cd61a6afc5dba339a03c7d7ae
Orlando Nelson <nelson8223@gmail.com>
2023-04-02 19:37:45 -0400

    updated install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,28 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    try {
+        const isDeferredPromptStored = sessionStorage.getItem('deferredPrompt');
+        if (isDeferredPromptStored) {
+            const deferredPrompt = JSON.parse(isDeferredPromptStored);
+            deferredPrompt.prompt();
+            const choiceResult = await deferredPrompt.userChoice;
+            console.log('User choice:', choiceResult.outcome);
+            sessionStorage.removeItem('deferredPrompt');
+        }
+    } catch (error) {
+        console.error('Error  prompts:', error);
+    }
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// TODO: Add a handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log('App was  installed');
+    navigator.serviceWorker.register('/service-worker.js');
+});
